30cd24863a520c76966980c522dd1be0820a773e
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-01-07 23:14:19 +0100

    handle W A S D input

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,23 @@
 #define SDL_MAIN_USE_CALLBACKS 1  // use the callbacks instead of main()
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h> // needed for callbacks
+#include <SDL3/SDL_events.h>
+#include <iostream>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+
+static enum keyCodes {
+    W = 119,
+    A = 97,
+    S = 115,
+    D = 100,
+};
+
+struct vec2D {
+    double x;
+    double y;
+} playerPosition;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -19,21 +33,45 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
+    playerPosition.x = 0,playerPosition.y = 0;
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
+
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  // end the program, reporting success to the OS
     }
+
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        switch (event->key.key) {
+            case W:
+                playerPosition.y--;
+                break;
+            case A:
+                playerPosition.x--;
+                break;
+            case S:
+                playerPosition.y++;
+                break;
+            case D:
+                playerPosition.x++;
+                break;
+        }
+    }
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    /* clear the window to the draw color. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderPoint(renderer, playerPosition.x, playerPosition.y);
 
     /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(renderer);
