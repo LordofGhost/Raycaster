fc42937a412f746d98a3e52ade806542b444cd1e
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-01-09 20:08:54 +0100

    create 3d renderer

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,8 +5,10 @@
 #include <iostream>
 #include <math.h>
 
-#define MOVE_DISTANCE 0.5
-#define PI 3.14159
+#define RENDER_HEIGHT 480
+#define RENDER_WIDTH 640
+#define RENDER_FOV 60
+#define MOVE_DISTANCE 0.2
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -40,26 +42,11 @@ int map[8][8] {
     { 1, 1, 1, 1, 1, 1, 1, 1 },
 };
 
-void drawMap() {
-    SDL_SetRenderDrawColor(renderer, 255,255,255,255);
-    for (int y = 0; y < 8; y++) {
-        for (int x = 0; x < 8; x++) {
-            if (map[y][x]) SDL_RenderPoint(renderer, x, y);
-        }
-    }
-}
-
 bool checkForWall(int x, int y) {
     return map[y][x] == 1;
 }
 
-void drawPlayer() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderLine(renderer, player.positionX, -player.positionY, player.positionX + (std::cos(player.angle) * 5), -(player.positionY + (std::sin(player.angle) * 5)));
-    //SDL_RenderPoint(renderer, player.positionX, -player.positionY);
-}
-
-double getWallDistance(double x, double y, double angel, int remainChecks) {
+double getWallDistance(double x, double y, double angle, int remainChecks) {
     v2D currentCell = { std::floor(x), std::floor(y)};
     v2D nextCell = {};
     double distance = 0;
@@ -72,40 +59,55 @@ double getWallDistance(double x, double y, double angel, int remainChecks) {
      * border of cell, considering the direction the angle points at
      */
     double xHypo;
-    if (std::cos(angel) != 0)
-        if (std::cos(angel) > 0)
-            xHypo = std::abs((1 - xNorm) / std::cos(angel));
+    if (std::cos(angle) != 0)
+        if (std::cos(angle) > 0)
+            xHypo = std::abs((1 - xNorm) / std::cos(angle));
         else
-            xHypo = std::abs(xNorm / std::cos(angel));
+            xHypo = std::abs(xNorm / std::cos(angle));
     else
         xHypo = std::numeric_limits<double>::infinity();
 
     double yHypo;
-    if (std::sin(angel) != 0)
-        if (std::sin(angel) > 0)
-            yHypo = std::abs(yNorm / std::sin(angel));
+    if (std::sin(angle) != 0)
+        if (std::sin(angle) > 0)
+            yHypo = std::abs(yNorm / std::sin(angle));
         else
-            yHypo = std::abs((1 - yNorm) / std::sin(angel));
+            yHypo = std::abs((1 - yNorm) / std::sin(angle));
     else
         yHypo = std::numeric_limits<double>::infinity();
 
     // move the border of the cell where the hypo is lower
     if (xHypo < yHypo) {
-        x = (cos(angel) >= 0) ? std::floor(x) + 1 : std::floor(x);
-        y += std::sin(angel) * xHypo;
+        x = (cos(angle) >= 0) ? std::floor(x) + 1 : std::floor(x);
+        y += std::sin(angle) * xHypo;
         distance += xHypo;
-        nextCell.x = (cos(angel) >= 0) ? currentCell.x + 1 : currentCell.x - 1;
+        nextCell.x = (cos(angle) >= 0) ? currentCell.x + 1 : currentCell.x - 1;
     } else {
-        x += std::cos(angel) * yHypo;
-        y = (sin(angel) >= 0) ? std::floor(y) : std::floor(y) + 1;
+        x += std::cos(angle) * yHypo;
+        y = (sin(angle) >= 0) ? std::floor(y) : std::floor(y) + 1;
         distance += yHypo;
-        nextCell.y = (sin(angel) >= 0) ? currentCell.y+1 : currentCell.y-1;
+        nextCell.y = (sin(angle) >= 0) ? currentCell.y+1 : currentCell.y-1;
     }
 
     remainChecks--;
-    if (!checkForWall(nextCell.x, nextCell.y) && remainChecks) return getWallDistance(x, y, angel, remainChecks) + distance;
+    if (!checkForWall(nextCell.x, nextCell.y) && remainChecks) return getWallDistance(x, y, angle, remainChecks) + distance;
 
     return distance;
+}
+
+int draw3dSpace(Player player) {
+
+    SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+    for (int renderColumn = 0; renderColumn < RENDER_WIDTH; renderColumn++) {
+        double angle = (player.angle - (RENDER_FOV * (M_PI / 180.0)) / 2) + (RENDER_FOV * (M_PI / 180.0) / RENDER_WIDTH) * renderColumn;
+        double distance = getWallDistance(player.positionX, player.positionY, angle, 16);
+
+        int distanceToHeight = (10 / distance) * 10;
+        int yTop = (RENDER_HEIGHT / 2) - (distanceToHeight / 2);
+        int yBottom = (RENDER_HEIGHT / 2) + (distanceToHeight / 2);
+        SDL_RenderLine(renderer, renderColumn, yTop, renderColumn, yBottom);
+    }
+    return 1;
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -122,7 +124,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    SDL_SetRenderScale(renderer, 10, 10);
+    //SDL_SetRenderScale(renderer, 10, 10);
     player.positionX = 0,player.positionY = 0, player.angle = 0;
 
     return SDL_APP_CONTINUE;
@@ -142,17 +144,16 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
                 player.positionY += std::sin(player.angle) * MOVE_DISTANCE;
                 break;
             case A:
-                player.angle += 0.1;
+                player.angle -= 0.1;
                 break;
             case S:
                 player.positionX -= std::cos(player.angle) * MOVE_DISTANCE;
                 player.positionY -= std::sin(player.angle) * MOVE_DISTANCE;
                 break;
             case D:
-                player.angle -= 0.1;
+                player.angle += 0.1;
                 break;
         }
-        std::cout << "Distance: " << getWallDistance(player.positionX, player.positionY, player.angle, 16) << std::endl;
     }
 
     return SDL_APP_CONTINUE;
@@ -163,8 +164,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    drawPlayer();
-    drawMap();
+    draw3dSpace(player);
 
     /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(renderer);
